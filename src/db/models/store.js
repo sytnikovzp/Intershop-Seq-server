32c1b8b239dc98b2465ab413f967895bee7b720c
alex <sytnikov.zp@gmail.com>
2024-07-16 20:12:10 +0300

    Generate model store

---
+++ src/db/models/store.js
@@ -0,0 +1,32 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Store extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Store.init(
+    {
+      title: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+      description: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: 'Store',
+      tableName: 'stores',
+      timestamps: false,
+      underscored: true,
+    }
+  );
+  return Store;
+};
