255159dd1983634a41a9db2fa7a07b43b0e5e334
alex <sytnikov.zp@gmail.com>
2024-07-16 20:03:07 +0300

    Generate model customer

---
+++ src/db/migrations/20240716161510-create-customer.js
@@ -0,0 +1,26 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('customers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      full_name: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('customers');
+  },
+};
---
+++ src/db/models/customer.js
@@ -0,0 +1,35 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Customer extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Customer.init(
+    {
+      full_name: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Customer',
+      tableName: 'customers',
+      timestamps: false,
+      underscored: true,
+    }
+  );
+  return Customer;
+};
