9bf46c87e03898b7d6c9892f9ec7d9030e4fe3de
alex <sytnikov.zp@gmail.com>
2024-07-16 20:18:49 +0300

    Generate model item_type

---
+++ src/db/models/item_type.js
@@ -0,0 +1,32 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Item_type extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Item_type.init(
+    {
+      title: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+      description: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: 'Item_type',
+      tableName: 'item_types',
+      timestamps: false,
+      underscored: true,
+    }
+  );
+  return Item_type;
+};
