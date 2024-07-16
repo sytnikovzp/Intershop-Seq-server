8386e27f4f9c97d10f50b8a2fdcdf001bcad5a32
alex <sytnikov.zp@gmail.com>
2024-07-16 20:24:58 +0300

    Generate model brand

---
+++ src/db/models/brand.js
@@ -0,0 +1,32 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Brand extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Brand.init(
+    {
+      title: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+      description: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: 'Brand',
+      tableName: 'brands',
+      timestamps: false,
+      underscored: true,
+    }
+  );
+  return Brand;
+};
